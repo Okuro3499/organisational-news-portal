8c381296f3ae1b560373877cd8e7abb6b3f6ab13
Gideon Okuro <gideonollonde@gmail.com>
2020-07-18 16:24:27 +0300

    Instantiate username getter method

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,6 +1,13 @@
 package models;
 
 public class User {
+    private String username;
+
     public User(String username, int departmentid, String role) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -6,9 +6,15 @@ import static org.junit.Assert.assertEquals;
 
 public class UserTest {
     @Test
-    public void userIntantiatesCorrectly() {
+    public void userInstantiatesCorrectly() {
         User user = new User ("Gideon",1, "Maintains System");
         assertEquals(true, user instanceof User);
+    }
+
+    @Test
+    public void getUsernameCorrectly() {
+        User user = new User ("Gideon",1, "Maintains System");
+        assertEquals("Gideon", user.getUsername());
     }
 
 }
