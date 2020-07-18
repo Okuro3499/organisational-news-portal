013cc6d8e41a1963b7bcd7a6eba3d3c8ef0d28d9
Gideon Okuro <gideonollonde@gmail.com>
2020-07-18 16:20:46 +0300

    correctly instantiate user instance

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,6 @@
 package models;
 
 public class User {
+    public User(String username, int departmentid, String role) {
+    }
 }
--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -1,5 +1,14 @@
 package models;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class UserTest {
+    @Test
+    public void userIntantiatesCorrectly() {
+        User user = new User ("Gideon",1, "Maintains System");
+        assertEquals(true, user instanceof User);
+    }
 
 }
