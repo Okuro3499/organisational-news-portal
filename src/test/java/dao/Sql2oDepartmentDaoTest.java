756dd8f36049e85cfdc34518399491c8dbe3e5c5
Gideon Okuro <gideonollonde@gmail.com>
2020-07-19 19:45:32 +0300

    pass get all and return empty list implementation methods

--- src/test/java/dao/Sql2oDepartmentDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentDaoTest.java
@@ -7,37 +7,54 @@ import org.junit.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class Sql2oDepartmentDaoTest {
     private Connection conn;
-    private Sql2oDepartmentDao DepartmentDao;
-    private Sql2oNewsDao NewsDao;
-    private Sql2oUserDao UserDao;
+    private Sql2oDepartmentDao departmentDao;
+    private Sql2oNewsDao newsDao;
+    private Sql2oUserDao userDao;
 
     @Before
     public void setUp() throws Exception {
         String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connectionString, "", "");
-        DepartmentDao = new Sql2oDepartmentDao(sql2o);
-        NewsDao= new Sql2oNewsDao(sql2o);
-        UserDao = new Sql2oUserDao(sql2o);
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        newsDao= new Sql2oNewsDao(sql2o);
+        userDao = new Sql2oUserDao(sql2o);
         conn = sql2o.open();
     }
 
     @After
     public void tearDown() throws Exception {
-        System.out.println("clearing database");
-        DepartmentDao.clearAll();
-        UserDao.clearAll();
-        NewsDao.clearAll();
+        conn.close();
     }
 
     @Test
     public void addingDepartmentSetsId() throws Exception {
-        Department newdepartment = new Department("Finance","Maintain Financial Records",13);
-        int originalDepartmentid = newdepartment.getId();
-        DepartmentDao.add(newdepartment);
-        assertNotEquals(originalDepartmentid, newdepartment.getId());
+        Department testDepartment = setupDepartment();
+        assertNotEquals(0, testDepartment.getId());
     }
+
+    @Test
+    public void addedDepartmentsAreReturnedFromGetAll() throws Exception {
+        Department testDepartment = setupDepartment();
+        assertEquals(1, departmentDao.getAll().size());
+    }
+
+    @Test
+    public void noDepartmentsReturnsEmptyList() throws Exception {
+        assertEquals(0, departmentDao.getAll().size());
+    }
+
+
+    //helpers
+    public Department setupDepartment() {
+        Department department = new Department("Finance","Maintain Financial Records",13);
+        departmentDao.add(department);
+        return department;
+    }
+
+
 }
