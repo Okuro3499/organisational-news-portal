014c3a3f3730a01a95bc4f50eaf877c6d0d3f4fa
Gideon Okuro <gideonollonde@gmail.com>
2020-07-19 19:25:50 +0300

    Generate setup and teardown methods

--- src/test/java/dao/Sql2oDepartmentDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentDaoTest.java
@@ -1,13 +1,31 @@
 package dao;
 
+import org.junit.After;
+import org.junit.Before;
 import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 public class Sql2oDepartmentDaoTest {
     private Connection conn;
-    private Sql2oDepartmentDao departmentDao;
-    private Sql2oNewsDao newDao;
-    private Sql2oUserDao userDao;
+    private Sql2oDepartmentDao DepartmentDao;
+    private Sql2oNewsDao NewsDao;
+    private Sql2oUserDao UserDao;
 
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        DepartmentDao = new Sql2oDepartmentDao(sql2o);
+        NewsDao= new Sql2oNewsDao(sql2o);
+        UserDao = new Sql2oUserDao(sql2o);
+        conn = sql2o.open();
+    }
 
-
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("clearing database");
+        DepartmentDao.clearAll();
+        UserDao.clearAll();
+        NewsDao.clearAll();
+    }
 }
