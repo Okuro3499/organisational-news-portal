0e93e0e84433e2f61274dce46d275e6631f1fae2
Gideon Okuro <gideonollonde@gmail.com>
2020-07-19 13:34:00 +0300

    create Sql2oUserDao file

---
+++ src/main/java/dao/Sql2oUserDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import models.User;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+public class Sql2oUserDao implements UserDao {
+    private final Sql2o sql2o;
+
+    public Sql2oUserDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+}
