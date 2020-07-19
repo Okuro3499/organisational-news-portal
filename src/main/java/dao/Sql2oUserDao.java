6f8b480bd98198267ff9674078290dc03d1e97d9
Gideon Okuro <gideonollonde@gmail.com>
2020-07-19 13:40:42 +0300

    implement add user Sql2oDao

--- src/main/java/dao/Sql2oUserDao.java
+++ src/main/java/dao/Sql2oUserDao.java
@@ -1,7 +1,9 @@
 package dao;
 
 import models.User;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
@@ -10,5 +12,19 @@ public class Sql2oUserDao implements UserDao {
 
     public Sql2oUserDao(Sql2o sql2o) {
         this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(User user) {
+        String sql = "INSERT INTO users (topic, description, departmentid) VALUES (:topic, :description, :departmentId)";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 }
