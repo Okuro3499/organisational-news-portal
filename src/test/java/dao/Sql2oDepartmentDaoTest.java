1f08889f46cffc41f85227259826c964dcba58b7
Gideon Okuro <gideonollonde@gmail.com>
2020-07-19 19:20:53 +0300

    create Sql2oDepartmenttest class

---
+++ src/test/java/dao/Sql2oDepartmentDaoTest.java
@@ -0,0 +1,13 @@
+package dao;
+
+import org.sql2o.Connection;
+
+public class Sql2oDepartmentDaoTest {
+    private Connection conn;
+    private Sql2oDepartmentDao departmentDao;
+    private Sql2oNewsDao newDao;
+    private Sql2oUserDao userDao;
+
+
+
+}
