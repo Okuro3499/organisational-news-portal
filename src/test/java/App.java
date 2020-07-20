b4dfa79f49d8b252159e6ac48e151a42259f0dd0
Gideon Okuro <gideonollonde@gmail.com>
2020-07-20 05:45:12 +0300

    import dependencies and add constructor

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -1,2 +1,26 @@
+import com.google.gson.Gson;
+import dao.Sql2oUserDao;
+import dao.Sql2oDepartmentDao;
+import dao.Sql2oNewsDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        Sql2oNewsDao newsDao;
+        Sql2oDepartmentDao departmentDao;
+        Sql2oUserDao userDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:~/Organisational-News-Portal.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+
+        departmentDao= new Sql2oDepartmentDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        userDao = new Sql2oUserDao(sql2o);
+        conn = sql2o.open();
+    }
 }
